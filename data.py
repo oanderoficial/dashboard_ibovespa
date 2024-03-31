c1b018ca1a13afc52e714afb5a9ea9c0cd2f8c97
Anderson B Silva (Ander) <32654298+oanderoficial@users.noreply.github.com>
2024-03-30 21:48:47 -0300

    Update data

--- data.py
+++ data.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 #pd.set_option('display.max_rows', None)
 #pd.set_option('display.max_columns', None)
-df_ibov = pd.read_csv('https://query1.finance.yahoo.com/v7/finance/download/%5EBVSP?period1=1674237073&period2=1705773073&interval=1d&events=history&includeAdjustedClose=true')
+df_ibov = pd.read_csv('https://query1.finance.yahoo.com/v7/finance/download/%5EBVSP?period1=1680223154&period2=1711845554&interval=1d&events=history&includeAdjustedClose=true')
 df_reversed = df_ibov.iloc[::-1]
 
